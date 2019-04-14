9072977b243a8fbcf82a01aefd7e9bab3d7de20e
scootlin <ts01518181@hotmail.com>
2019-04-14 11:06:05 +0800

    Add Remove Duplicates from Sorted Array

---
+++ Remove Duplicates from Sorted Array.go
@@ -0,0 +1,16 @@
+func removeDuplicates(nums []int) int {
+    // list := make(map[int]int)
+    if len(nums) <= 1{
+        return len(nums)
+    }
+    // nlen := 0
+    i := 0
+    for i < len(nums)-1{
+        if nums[i] == nums[i+1]{
+            nums = append(nums[:i],nums[i+1:]...)
+        }else{
+            i++
+        }
+    }
+    return len(nums)
+}

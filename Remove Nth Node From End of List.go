37c0d134cbe282ac84a60e0eef607a8d6a1a894f
scootlin <ts01518181@hotmail.com>
2019-04-14 11:04:29 +0800

    Add Remove Nth Node From End of List

---
+++ Remove Nth Node From End of List.go
@@ -0,0 +1,20 @@
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    s := size(head)
+    n = s - n
+    if n == 0 {
+        return head.Next
+    }
+    h := head
+    for i := 0; i < n - 1; i++ {
+        h = h.Next
+    }
+    h.Next = h.Next.Next
+    return head
+}
+func size(head *ListNode) int {
+    size := 0
+    for h := head; h != nil; h = h.Next {
+        size++
+    }
+    return size
+}

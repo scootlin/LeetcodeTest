b832629584a6e8fe550c288391470e4e737f9fbd
scootlin <ts01518181@hotmail.com>
2019-04-14 11:09:23 +0800

    Add Convert a Number to Hexadecimal

---
+++ Convert a Number to Hexadecimal.go
@@ -0,0 +1,20 @@
+func toHex(num int) string {
+    if num == 0{
+        return "0"
+    }
+	var n uint64
+	if num < 0 {
+		n = 1<<32 + uint64(num)
+	} else {
+		n = uint64(num)
+	}
+	hex := "0123456789abcdef"
+	var s string
+	for n > 0 {
+		a := n % 16
+		fmt.Println(string(hex[a]))
+		s = string(hex[a]) + s
+		n /= 16
+	}
+	return s
+}

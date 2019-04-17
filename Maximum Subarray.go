16fd1e2692d3766b0814b3db5a9a11c7eea426d8
scootlin <ts01518181@hotmail.com>
2019-04-17 09:29:02 +0800

    Create Maximum Subarray.go

---
+++ Maximum Subarray.go
@@ -0,0 +1,18 @@
+func maxSubArray(nums []int) int {
+    if len(nums) <= 1{
+        return nums[0]
+    }
+    max := -1<<31
+    num := 0
+    lnums := len(nums)
+    for i:=0; i<lnums; i++{
+        num += nums[i]
+        if num < nums[i]{
+            num = nums[i]
+        }
+        if max < num{
+            max = num
+        }
+    }
+    return max
+}

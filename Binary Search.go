fa15670a086b70011b46c64f3baaf6223da6b394
scootlin <ts01518181@hotmail.com>
2019-04-17 18:19:22 +0800

    Create Binary Search.go

---
+++ Binary Search.go
@@ -0,0 +1,18 @@
+func search(nums []int, target int) int {
+    start := 0
+    numsl:=len(nums)
+    end :=numsl-1
+    i := numsl / 2
+    for start <= end{
+        fmt.Println(start,i ,end)
+        if nums[i] == target{
+            return i
+        }else if nums[i] > target{
+            end = i-1 
+        }else if nums[i] < target{
+            start = i+1
+        }
+        i = (end+start) / 2
+    }
+    return -1
+}

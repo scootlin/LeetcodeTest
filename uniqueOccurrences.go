a53bebe7fe831995f6ce3be0a8f696e42d30d904
scootlin <ts01518181@hotmail.com>
2019-10-05 10:42:27 +0800

    Create uniqueOccurrences.go

---
+++ uniqueOccurrences.go
@@ -0,0 +1,26 @@
+func uniqueOccurrences(arr []int) bool {
+    sort.Ints(arr)
+    count := make(map[int]int)
+    val := make(map[int]int)
+    prev := arr[0]
+    c := 0
+    for _,v := range arr{  
+        val[v]++
+        //fmt.Printf("V = %d, prev = %d ,count = %d\n",v,prev,val[v])
+        if v != prev{
+            c = val[prev]
+            if _,ok := count[c]; ok{
+                return false
+            }else{
+                count[c] = prev
+            }
+        }
+        prev = v        
+    }
+    if _,ok := count[val[arr[len(arr)-1]]]; ok{
+       return false
+    }
+    fmt.Println(count)
+    fmt.Println(val)
+    return true
+}uniqueOccurrences

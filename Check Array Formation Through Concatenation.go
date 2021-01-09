a0bf6276150e8246dbeb0d1f9837d3da86fb4068
scootlin <ts01518181@hotmail.com>
2021-01-09 13:15:49 +0900

    Create Check Array Formation Through Concatenation.go

---
+++ Check Array Formation Through Concatenation.go
@@ -0,0 +1,24 @@
+func canFormArray(arr []int, pieces [][]int) bool {
+    for _, piece := range pieces{
+        aindex := -1
+        for i, v := range arr{
+            if v == piece[0]{
+                aindex = i
+            }
+        }
+        if aindex == -1 {
+            return false
+        }
+        if len(piece) < 2{
+            continue
+        }
+        for _,v := range piece{
+            if aindex < len(arr) && arr[aindex] == v{
+                aindex++
+            }else{
+                return false
+            }
+        }
+    }
+    return true
+}
